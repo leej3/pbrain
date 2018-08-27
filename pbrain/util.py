616fa28f0203f47a21bf8c5ef82be0e66dbf5d81
leej3 <johnleenimh@gmail.com>
2018-08-27 19:19:22 -0400

    add util functions

---
+++ pbrain/util.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Utilities."""
+import numpy as np
+import nibabel as nib
+import tensorflow as tf
+import pandas as pd
+# sys.excepthook = lambda exctype,exc,traceback : print("{}: {}".format(exctype.__name__,exc))
+def clean_csv(input_csv,output_csv):
+	df = pd.read_csv(input_csv)
+	df['loads'] = df[df.columns[0]].apply(check_nibload)
+	df.query('loads').to_csv(output_csv,index=False,sep=',')
+
+def check_nibload(input_path):
+
+	try:
+		nib.load(input_path).get_data()
+	except Exception as e:
+		print(e)
+		print("Failure: ", input_path)
+		return False
+	return True
+
+	
+
+def zscore(a):
+    """Return array of z-scored values."""
+    a = np.asarray(a)
+    return (a - a.mean()) / a.std()
+
+
+def setup_exceptionhook():
+    """
+    Overloads default sys.excepthook with our exceptionhook handler.
+
+    If interactive, our exceptionhook handler will invoke pdb.post_mortem;
+    if not interactive, then invokes default handler.
+    """
+    def _pdb_excepthook(type, value, tb):
+        if is_interactive():
+            import traceback
+            import pdb
+            traceback.print_exception(type, value, tb)
+            # print()
+            pdb.post_mortem(tb)
+        else:
+            lgr.warning(
+              "We cannot setup exception hook since not in interactive mode")
+
+    sys.excepthook = _pdb_excepthook
+
