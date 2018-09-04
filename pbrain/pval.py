11088672ff192acf876895c8f7af7231ace69b58
leej3 <johnleenimh@gmail.com>
2018-09-04 17:12:19 -0400

    add pval functionality

---
+++ pbrain/pval.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import nibabel as nib
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from pbrain.models.vae3d import autoencoder
+from pbrain.predict import predict
+from pbrain.util import get_loss,csv_to_batches, get_image
+from pathlib import Path
+# from pbrain.util import zscore
+
+
+
+def pval(model_dir,input_csv,output_csv,reference_csv,output_dir):
+    
+	df = pd.read_csv(input_csv)
+	if not 'score' in df.columns:
+    	predict(model_dir,input_csv,output_csv,output_dir)
+    	df = pd.read_csv(output_csv)
+    
+    	df is a dataframe with image path and scores. build empirical dist. from input_csv
