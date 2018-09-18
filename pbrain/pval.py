557505d69a823442bd159ba1b4039191c20c65d7
Charles Zheng <charles.y.zheng@gmail.com>
2018-09-18 11:26:16 -0400

    pval code

--- pbrain/pval.py
+++ pbrain/pval.py
@@ -17,7 +17,7 @@ import scipy as sp
 
 
 
-def pval(model_dir,input_csv,output_csv,reference_csv=None,reference_table=None,table_path_out='reference_table.npy',output_dir=None):
+def pval(model_dir,input_csv,output_csv,reference_csv=None,output_dir=None):
     # Load test scores
     test_df = pd.read_csv(input_csv)
     if not 'score' in test_df.columns:
@@ -25,29 +25,19 @@ def pval(model_dir,input_csv,output_csv,reference_csv=None,reference_table=None,
         test_df = pd.read_csv(output_csv)
     test_scores = test_df['score'].values
 
-    assert reference_csv or reference_table
-    if reference_table:
-        rtable = np.load(reference_table)
-    elif reference_csv:
-        # read csv, convert to kde
-        # Load training scores
-        train_df = pd.read_csv(reference_csv)
-        scores = train_df['score'].values # scores for training data
-        # Choose bandwidth for Kernel Density Estimation
-        iqr = sp.stats.iqr(scores) # use IQR to set maximum bandwidth for KDE
-        params = {'bandwidth': np.logspace(-2, 0, 20) * iqr}
-        grid = GridSearchCV(KernelDensity(), params, cv = 20)
-        grid.fit(scores.reshape((-1,1)))
-        bd = grid.best_estimator_.bandwidth
-        # Generate reference table
-        a,b = np.percentile(scores, [0.1, 99.9])
-        xs = np.linspace(a, b, 100)
-        # Compute lower tails on reference using KDE with Gaussian kernel
-        diffs = np.dot(xs.reshape((-1,1)), np.ones((1, len(scores)))) - np.dot(np.ones((len(xs), 1)), scores.reshape((1,-1))) # a n_test x n_train matrix of differences between xs and training points
-        pvals = np.mean(1-norm.cdf(diffs, loc = 0, scale = bd), 1) # the p-values obtained by averaging upper tails of all training points for each grid point
-        rtable = np.array([xs, np.log(pvals)]).T
-        np.save(table_path_out, rtable)
-    
-    #compute pvals from reference table
-    pvals = np.exp(sp.interp(test_scores, rtable[:,0], rtable[:, 1]))
-    return pvals
+    train_df = pd.read_csv(reference_csv)
+    scores = train_df['score'].values # scores for training data
+    # Choose bandwidth for Kernel Density Estimation
+    iqr = sp.stats.iqr(scores) # use IQR to set maximum bandwidth for KDE
+    params = {'bandwidth': np.array([0.1, 0.2, 0.3, 0.4]) * iqr}
+    grid = GridSearchCV(KernelDensity(), params, cv = 4)
+    grid.fit(scores.reshape((-1,1)))
+    bd = grid.best_estimator_.bandwidth
+    # Generate reference table
+    a,b = np.percentile(scores, [0.1, 99.9])
+    xs = test_scores
+    diffs = np.dot(xs.reshape((-1,1)), np.ones((1, len(scores)))) - np.dot(np.ones((len(xs), 1)), scores.reshape((1,-1))) # a n_test x n_train matrix of differences between xs and training points
+    pvals = np.mean(1-norm.cdf(diffs, loc = 0, scale = bd), 1) # the p-values obtained by averaging upper tails of all training points for each grid point
+    #return pvals
+    df['pvalue'] = pvals
+    df.to_csv(output_csv)
