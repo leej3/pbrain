improve bd calculation and fix pval

pbrain/pval.py
@@ -31,11 +31,11 @@ def pval(input_csv,output_csv,reference_csv=None):
     # Choose bandwidth for Kernel Density Estimation
     iqr = sp.stats.iqr(scores) # use IQR to set maximum bandwidth for KDE
 
-    bd = iqr / 4
+    bd = np.std(scores) * len(scores)**(-0.2)
     print("Computing pvals...")
     # Compute lower tails on test data using KDE with Gaussian kernel
     diffs = np.dot(test_scores.reshape((-1,1)), np.ones((1, len(scores)))) - np.dot(np.ones((len(test_scores), 1)), scores.reshape((1,-1))) # a n_test x n_train matrix of differences between test and training points
-    pvals = np.mean(norm.cdf(diffs, loc = 0, scale = bd), 1) # the p-values obtained by averaging lower tails of all training points for each test point
+    pvals = 1 - np.mean(norm.cdf(diffs, loc = 0, scale = bd), 1) # the p-values obtained by averaging lower tails of all training points for each test point
     test_df['pval'] = pvals
     print("Writing csv...")
     test_df.to_csv(output_csv,index = False)